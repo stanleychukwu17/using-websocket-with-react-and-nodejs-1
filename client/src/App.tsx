b6412025556fbbeea53f81974c01bb6ffc67410e
Chukwu Stanley <stanleychukwu17@yahoo.com>
2025-03-28 12:54:35 +0100

    added the logic to render login component if the user is not logged in, otherwise would render the home component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,19 @@
+import { useState } from "react";
+import Login from "./components/Login";
+import Home from "./components/Home";
 
 export default function App1() {
+    const [username, setUsername] = useState<string>('')
+
     return (
-        <div>App1</div>
+        <div>
+            <div className="">
+                <h2>Hello {username}</h2>
+            </div>
+            <div className="">
+                {username && <Home username={username} />}
+                {!username && <Login onSubmit={setUsername} />}
+            </div>
+        </div>
     )
 }
