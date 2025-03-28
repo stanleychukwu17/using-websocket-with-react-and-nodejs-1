1a1e1b9d2a6e76b575483e8ed0604b1b7b823cb1
Chukwu Stanley <stanleychukwu17@yahoo.com>
2025-03-28 16:42:47 +0100

    completed the displaying of cursor position for every user connected to the websocket

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,37 +1,70 @@
-import { useEffect } from "react"
+import { useCallback, useEffect, useState } from "react"
 import useWebSocket from "react-use-websocket"
+import throttle  from "lodash.throttle"
 
 type HomeProps = {
     username: string
 }
-
-const mouseMove = (event: MouseEvent) => {
-    console.log(event.clientX, event.clientY)
+type receivedMessagesType = {
+    [uuid: string] : {
+        username: string,
+        cursor: {
+            x: number,
+            y: number
+        }
+    }
 }
 
-export default function Home({username}: HomeProps) {
+// throttle time in ms, we use 50ms for a smother experience
+const throttleTime = 50
 
+
+export default function Home({username}: HomeProps) {
+    const [messages, setMessages] = useState<receivedMessagesType>({})
     const WS_URL = "ws://localhost:9000"
 
     const { sendJsonMessage } = useWebSocket(WS_URL, {
         share: true,
         queryParams: { username },
-        onMessage: (event) => console.log(event.data),
+        onMessage: (event) => {
+            const newUpdates = JSON.parse(event.data)
+
+            // console.log("message received", newUpdates)
+            setMessages(newUpdates)
+        },
         shouldReconnect: () => true,
         reconnectInterval: 1000,
         reconnectAttempts: 20,
     })
 
+    const sendJsonMessageThrottled = useCallback(
+        throttle( (message) => sendJsonMessage(message), throttleTime),
+        [sendJsonMessage]
+    )
+
     useEffect(() => {
-        console.log(sendJsonMessage)
+        const mouseMove = (event: MouseEvent) => {
+            sendJsonMessageThrottled({x: event.clientX, y: event.clientY})
+        }
+    
         window.addEventListener('mousemove', mouseMove)
 
         return () => {
             window.removeEventListener('mousemove', mouseMove)
         }
-    }, [sendJsonMessage])
+    }, [sendJsonMessageThrottled])
 
     return (
-        <div>welcome back boy</div>
+        <div>
+            {Object.keys(messages).map((uuid) => {
+                return (
+                    <div key={uuid}>
+                        <div>Name: {messages[uuid].username}</div>
+                        <div>X: {messages[uuid].cursor.x}</div>
+                        <div>Y: {messages[uuid].cursor.y}</div>
+                    </div>
+                )
+            })}
+        </div>
     )
 }
